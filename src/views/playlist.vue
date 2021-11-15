f81e0cf3cffef835b3a7901f9dacf4288ab0b62f
AirHua-byte <3301833942@qq.com>
2021-11-15 22:08:59 +0800

    playlist

--- src/views/playlist.vue
+++ src/views/playlist.vue
@@ -1,6 +1,93 @@
 <template>
   <div>
-    playlist
+    <div class="playlist" v-show="show">
+      <div
+        v-if="specialPlaylistInfo === undefined && !isLikeSongsPage"
+        class="playlist-info"
+      >
+        <Cover
+          :id="playlist.id"
+          :image-url="playlist.coverImgUrl | resizeImage(1024)"
+          :show-play-button="true"
+          :always-show-shadow="true"
+          :click-cover-to-play="true"
+          :fixed-size="288"
+          type="playlist"
+          :cover-hover="false"
+          :play-button-size="18"
+          @click.right.native="openMenu"
+        >
+        </Cover>
+        <div class="info">
+          <div class="title" @click.right="openMenu">
+            <span v-if="playlist.privacy === 10" class="lock-icon">
+              <svg-icon icon-class="lock"></svg-icon>
+            </span>
+            {{ playlist.name }}
+          </div>
+          <div class="artist">
+            Playlist by
+            <span
+              v-if="
+              [
+                5277771961,
+                5277965913,
+                5277969451,
+                5277778542,
+                5278068783,
+              ].includes(playlist.id)
+              "
+              style="font-weight: 600"
+            >
+              Apple Music
+            </span>
+            <a
+              v-else
+              :href="`https://music.163.com/#/user/home?id=${playlist.creator.userId}`"
+              target="blank"
+            >
+              {{ playlist.creator.nickname }}
+            </a>
+          </div>
+          <div class="date-and-count">
+            {{ $t('playlist.updatedAt') }}
+            {{ playlist.updateTime | formatDate }} · {{ playlist.trackCount }}
+            {{ $t('common.songs') }}
+          </div>
+          <div class="description" @click="toggleFullDescription">
+            {{ playlist.description }}
+          </div>
+          <div class="buttons">
+            <ButtonTwoTone
+              icon-class="play"
+              @click.native="playPlaylistByID()"
+            >
+              {{ $t('common.play') }}
+            </ButtonTwoTone>
+            <ButtonTwoTone
+              v-if="playlist.creator.userId !== data.user.userId"
+              :icon-class="playlist.subscribed ? 'heart-solid' : 'heart'"
+              :icon-button="true"
+              :horizontal-padding="0"
+              :color="playlist.subscribed ? 'blue' : 'grey'"
+              :text-color="playlist.subscribed ? '#335eea' : ''"
+              :background-color="
+                playlist.subscribed ? 'var(--color-secondary-bg)' : ''
+              "
+              @click.native="likePlaylist"
+            >
+            </ButtonTwoTone>
+            <ButtonTwoTone
+              icon-class="more"
+              :icon-button="true"
+              :horizontal-padding="0"
+              color="grey"
+              @click.native="openMenu"
+            ></ButtonTwoTone>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,19 +96,19 @@ import { mapMutations, mapActions, mapState } from 'vuex';
 import NProgress from 'nprogress';
 import {
   getPlaylistDetail,
-  subscribePlaylist,
-  deletePlaylist,
+  // subscribePlaylist,
+  // deletePlaylist,
 } from '@/api/playlist';
-import { getTrackDetail } from '@/api/track';
-import { isAccountLoggedIn } from '@/utils/auth';
-import nativeAlert from '@/utils/nativeAlert';
-import locale from '@/locale';
+// import { getTrackDetail } from '@/api/track';
+// import { isAccountLoggedIn } from '@/utils/auth';
+// import nativeAlert from '@/utils/nativeAlert';
+// import locale from '@/locale';
 
 import ButtonTwoTone from '@/components/ButtonTwoTone.vue';
-import ContextMenu from '@/components/ContextMenu.vue';
-import TrackList from '@/components/TrackList.vue';
+// import ContextMenu from '@/components/ContextMenu.vue';
+// import TrackList from '@/components/TrackList.vue';
 import Cover from '@/components/Cover.vue';
-import Modal from '@/components/Modal.vue';
+// import Modal from '@/components/Modal.vue';
 
 const specialPlaylist = {
   2829816518: {
@@ -115,9 +202,9 @@ export default {
   components: {
     Cover,
     ButtonTwoTone,
-    ContextMenu,
-    TrackList,
-    Modal,
+    // ContextMenu,
+    // TrackList,
+    // Modal,
   },
   directives: {
     focus: {
@@ -184,5 +271,134 @@ export default {
       );
     },
   },
-}
+  created() {
+    if (this.$route.name === 'likedSongs') {
+      this.loadData(this.data.likedSongPlaylistID);
+    } else {
+      this.loadData(this.$route.params.id);
+    }
+    setTimeout(() => {
+      if (!this.show) NProgress.start();
+    }, 1000);
+  },
+  methods: {
+    ...mapMutations(['appendTrackToPlayerList']),
+    ...mapActions(['playFirstTrackOnlist', 'playTrackOnListByID', 'showToast']),
+    playPlaylistByTD(trackID = 'first') {
+      console.log(trackID);
+    },
+    likePlaylist(toast = false) {
+      console.log(toast);
+    },
+    loadData(id, next = undefined) {
+      this.id = id;
+      getPlaylistDetail(this.id, true)
+        .then(data => {
+          this.playlist = data.playlist;
+          this.tracks = data.playlist.tracks;
+          NProgress.done();
+          if (next !== undefined) next();
+          this.show = true;
+          this.lastLoadedTrackIndex = data.playlist.tracks.length - 1;
+          return data;
+        })
+        .then(() => {
+          if (this.playlist.trackCount > this.tracks.length) {
+            this.loadingMore = true;
+            this.loadMore();
+          }
+        })
+    },
+    loadMore(loadNum = 100) {
+      console.log(loadNum);
+    },
+    openMenu() {
+
+    },
+    deletePlaylist() {
+
+    },
+    editPlaylist() {
+
+    },
+    searchInPlaylist() {
+
+    },
+    removeTrack() {
+
+    },
+    inputDebounce() {
+
+    },
+    toggleFullDescription() {
+
+    },
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.playlist {
+  margin-top: 32px;
+}
+.playlist-info {
+  display: flex;
+  margin-bottom: 72px;
+  position: relative;
+  .info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1;
+    margin-left: 56px;
+    .title {
+      font-size: 36px;
+      font-weight: 700;
+      color: var(--color-text);
+      .lock-icon {
+        opacity: 0.28;
+        color: var(--color-text);
+        margin-right: 8px;
+        .svg-icon {
+          height: 26px;
+          width: 26px;
+        }
+      }
+    }
+    .artist {
+      font-size: 18px;
+      opacity: 0.88;
+      color: var(--color-text);
+      margin-top: 24px;
+    }
+    .data-and-count {
+      font-size: 14px;
+      opacity: 0.68;
+      color: var(--color-text);
+      margin-top: 2px;
+    }
+    .description {
+      font-size: 14px;
+      opacity: 0.68;
+      color: var(--color-text);
+      margin-top: 24px;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 3;
+      overflow: hidden;
+      cursor: pointer;
+      &:hover {
+        transition: opacity 0.3s;
+        opacity: 0.88;
+      }
+    }
+    .buttons {
+      margin-top: 32px;
+      display: flex;
+      button {
+        margin-right: 16px;
+      }
+    }
+  }
+}
+</style>
